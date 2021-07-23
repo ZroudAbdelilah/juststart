997ae27d1e2b384980732cb4b131439d530a4045
ZroudAbdelilah <76938527+ZroudAbdelilah@users.noreply.github.com>
2021-07-23 14:23:45 +0100

    finaliser les controllers et  declarer les route

---
+++ Fil_Rouge/app/Http/Controllers/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        return USER::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return USER::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $user= USER::find($id);
+        $user->update($request->all());
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        return USER::destroy($id);
+    }
+}
--- Fil_Rouge/routes/api.php
+++ Fil_Rouge/routes/api.php
@@ -1,5 +1,11 @@
 <?php
-
+use App\Http\Controllers\AdressController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ProjectLeaderController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +20,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::resource('adresss', AdressController::class);
+Route::resource('categorys', CategoryController::class);
+Route::resource('comments', CommentController::class);
+Route::resource('orders', OrderController::class);
+Route::resource('projects', ProjectController::class);
+Route::resource('project_leader', ProjectLeaderController::class);
+Route::resource('tags', TagController::class);
+Route::resource('users', UserController::class);
+
+
+
+
+
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
